ef5a34232254f0b5c6d510ff92708b985933bde7
ramenryoko <lucas@digitalghost.org>
2026-05-08 18:35:52 -0700

    Updated to handle 377 cache

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -99,7 +99,7 @@ public class Game extends GameShell {
 
     public static void main(String[] args) throws UnknownHostException {
         System.setProperty("java.net.preferIPv6Addresses", "true");
-        System.out.println("RS2 user client - release #" + 317 + "(Modified to 377)");
+        System.out.println("RS2 user client - release #" + 317 + " (Modified to 377)");
 
         nodeID = 10;
         portOffset = 0;
